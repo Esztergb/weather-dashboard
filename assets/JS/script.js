e2be7d04b7a486d16c8f25283b54c297554def92
Esztergb <eszterbarbuscia@Eszters-MacBook-Air.local>
2023-05-10 12:23:32 -0700

    simplified the code for displaying date with the help of a tutor

--- assets/JS/script.js
+++ assets/JS/script.js
@@ -3,31 +3,36 @@ var today = dayjs();
 $(".todaysdate").text(today.format("MMM D, YYYY"));
 // console.log(today)
 
-var dayTwo = dayjs().add(1, "days");
-// console.log(dayTwo)
-$("#date0").text(dayTwo.format("MMM D, YYYY"));
+// var dayTwo = dayjs().add(1, "days");
+// // console.log(dayTwo)
+// $("#date0").text(dayTwo.format("MMM D, YYYY"));
 
-var dayThree = dayjs().add(2, "days");
-// console.log(dayTwo)
-$("#date1").text(dayThree.format("MMM D, YYYY"));
+// var dayThree = dayjs().add(2, "days");
+// // console.log(dayTwo)
+// $("#date1").text(dayThree.format("MMM D, YYYY"));
 
-var dayFour = dayjs().add(3, "days");
-// console.log(dayTwo)
-$("#date2").text(dayFour.format("MMM D, YYYY"));
+// var dayFour = dayjs().add(3, "days");
+// // console.log(dayTwo)
+// $("#date2").text(dayFour.format("MMM D, YYYY"));
 
-var dayFive = dayjs().add(4, "days");
-// console.log(dayTwo)
-$("#date3").text(dayFive.format("MMM D, YYYY"));
+// var dayFive = dayjs().add(4, "days");
+// // console.log(dayTwo)
+// $("#date3").text(dayFive.format("MMM D, YYYY"));
 
-var daySix = dayjs().add(5, "days");
-// console.log(dayTwo)
-$("#date4").text(daySix.format("MMM D, YYYY"));
+// var daySix = dayjs().add(5, "days");
+// // console.log(dayTwo)
+// $("#date4").text(daySix.format("MMM D, YYYY"));
+
+for (let i = 0; i < 5; i++) {
+  var day = dayjs().add(i+1, "days");
+  $(`#date${i}`).text(day.format("MMM D, YYYY"));
+}
 
 // ===============================================
-var city='';
-var url='';
-var apiKey='';
-var cueryUrl ='';
+var city = "";
+var url = "";
+var apiKey = "";
+var cueryUrl = "";
 var citiesDiv = document.getElementById("searched_cities_container");
 var cities = []; //start with empty array
 
@@ -37,30 +42,30 @@ searchClicker();
 
 //function to pull saved cities from local storage and add them to empty arrey
 
-function init(){
-  var saved_cities = JSON.parse(localStorage.getItem('cities'));
+function init() {
+  var saved_cities = JSON.parse(localStorage.getItem("cities"));
 
-  if (saved_cities !== null){
-    cities = saved_cities
+  if (saved_cities !== null) {
+    cities = saved_cities;
   }
   renderButtons(); //call the event to show the past searched cities
 }
 
 //set local storage
-function storeCities(){
+function storeCities() {
   localStorage.setItem("cities", JSON.stringify(cities));
 }
 
-function renderButtons(){
-  citiesDiv.innerHTML = '';
-  if(cities == null){
+function renderButtons() {
+  citiesDiv.innerHTML = "";
+  if (cities == null) {
     return;
   }
   var unique_cities = [...new Set(cities)];
-  for(var i=0; i < unique_cities.length; i++){
+  for (var i = 0; i < unique_cities.length; i++) {
     var cityName = unique_cities[i];
 
-    var buttonEL = document.createElement('button');
+    var buttonEL = document.createElement("button");
     buttonEL.textContent = cityName;
     buttonEL.setAttribute("class", "listbtn");
 
@@ -70,41 +75,41 @@ function renderButtons(){
 }
 
 //on click function for search history buttons
-function listClicker(){
-  $(".listbtn").on("click", function(event){
-    console.log("anybody home")
+function listClicker() {
+  $(".listbtn").on("click", function (event) {
+    console.log("anybody home");
     event.preventDefault();
     console.log("hello");
     city = $(this).text().trim();
     APIcalls();
-  })
+  });
 }
 
 //on click function for main search button
 
 function searchClicker() {
-  $("#searchbtn").on("click", function(event){
+  $("#searchbtn").on("click", function (event) {
     event.preventDefault();
-    city=$(this).prev().val().trim()
+    city = $(this).prev().val().trim();
 
     //push the user enterd citi into the cities array
     cities.push(city);
     //city array.lengh never exceed 8
-    if(cities.length > 8){
-      cities.shift()
+    if (cities.length > 8) {
+      cities.shift();
     }
-    if (city ==""){
+    if (city == "") {
       return;
     }
     APIcalls();
     storeCities();
     renderButtons();
-  })
+  });
 }
 
 //API calls  var apiKey = "bbaae16b1501b9e43700f50808acd154";
 
-function APIcalls(){
+function APIcalls() {
   url = "https://api.openweathermap.org/data/2.5/forecast?q=";
   currenturl = "https://api.openweathermap.org/data/2.5/weather?q=";
   APIkey = "&appid=bbaae16b1501b9e43700f50808acd154";
@@ -115,18 +120,18 @@ function APIcalls(){
     url: queryurl,
     method: "GET",
   }).then(function (response) {
-    let day_number = 0;
+    var day_number = 0;
 
     //iterate through the 40 weather data sets
-    for (let i = 0; i < response.list.length; i++) {
+    for (var i = 0; i < response.list.length; i++) {
       //split function to isolate the time from the time/data aspect of weather data, and only select weather reports for 3pm
       if (response.list[i].dt_txt.split(" ")[1] == "15:00:00") {
         //if time of report is 3pm, populate text areas accordingly
-        let day = response.list[i].dt_txt.split("-")[2].split(" ")[0];
-        let month = response.list[i].dt_txt.split("-")[1];
-        let year = response.list[i].dt_txt.split("-")[0];
+        var day = response.list[i].dt_txt.split("-")[2].split(" ")[0];
+        var month = response.list[i].dt_txt.split("-")[1];
+        var year = response.list[i].dt_txt.split("-")[0];
         $("#" + day_number + "date").text(month + "/" + day + "/" + year);
-        let temp = Math.round(
+        var temp = Math.round(
           ((response.list[i].main.temp - 273.15) * 9) / 5 + 32
         );
         $("#" + day_number + "five_day_temp").text(
@@ -155,7 +160,7 @@ function APIcalls(){
     method: "GET",
   }).then(function (current_data) {
     console.log(current_data);
-    let temp = Math.round(((current_data.main.temp - 273.15) * 9) / 5 + 32);
+    var temp = Math.round(((current_data.main.temp - 273.15) * 9) / 5 + 32);
     console.log("The temperature in " + city + " is: " + temp);
     $("#today_temp").text(
       "Temperature: " + temp + String.fromCharCode(176) + "F"
@@ -173,46 +178,30 @@ function APIcalls(){
   });
 }
 
+// function getApi(){
+//   var requestUrl =
+//     "https://api.openweathermap.org/data/2.5/forecast?lat=51.5085&lon=-0.1257&appid=bbaae16b1501b9e43700f50808acd154";
 
+//   fetch(requestUrl)
+//     .then(function (response) {
+//       return response.json();
+//     })
+//     .then(function (data) {
+//       console.log(data);
+//     });
+// }
+// getApi()
 
+// function getCity() {
+//   var requestUrl =
+//     "https://api.openweathermap.org/data/2.5/forecast?q=London&appid=bbaae16b1501b9e43700f50808acd154";
 
-
-  
-  
-  
-  
-  
-  // function getApi(){
-  //   var requestUrl =
-  //     "https://api.openweathermap.org/data/2.5/forecast?lat=51.5085&lon=-0.1257&appid=bbaae16b1501b9e43700f50808acd154";
-
-  //   fetch(requestUrl)
-  //     .then(function (response) {
-  //       return response.json();
-  //     })
-  //     .then(function (data) {
-  //       console.log(data);
-  //     });
-  // }
-  // getApi()
-
-
-
-  // function getCity() {
-  //   var requestUrl =
-  //     "https://api.openweathermap.org/data/2.5/forecast?q=London&appid=bbaae16b1501b9e43700f50808acd154";
-
-  //   fetch(requestUrl)
-  //     .then(function (response) {
-  //       return response.json();
-  //     })
-  //     .then(function (data) {
-  //       console.log(data);
-  //     });
-  // }
-  // getCity()
-
-
-
-
-
+//   fetch(requestUrl)
+//     .then(function (response) {
+//       return response.json();
+//     })
+//     .then(function (data) {
+//       console.log(data);
+//     });
+// }
+// getCity()
