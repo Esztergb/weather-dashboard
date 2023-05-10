12e7224ca7ec43a9586e8be96835d57a59bbb728
Esztergb <eszterbarbuscia@Eszters-MacBook-Air.local>
2023-05-09 17:59:25 -0700

    added functions to my script file

--- assets/JS/script.js
+++ assets/JS/script.js
@@ -1,6 +1,6 @@
 //============dates displayed with Day JS=========
 var today = dayjs();
-$(".todaysdate").text(today.format("[Today is] MMM D, YYYY"));
+$(".todaysdate").text(today.format("MMM D, YYYY"));
 // console.log(today)
 
 var dayTwo = dayjs().add(1, "days");
@@ -51,42 +51,166 @@ function storeCities(){
   localStorage.setItem("cities", JSON.stringify(cities));
 }
 
-//API calls
+function renderButtons(){
+  citiesDiv.innerHTML = '';
+  if(cities == null){
+    return;
+  }
+  var unique_cities = [...new Set(cities)];
+  for(var i=0; i < unique_cities.length; i++){
+    var cityName = unique_cities[i];
 
-var apiKey = "bbaae16b1501b9e43700f50808acd154";
+    var buttonEL = document.createElement('button');
+    buttonEL.textContent = cityName;
+    buttonEL.setAttribute("class", "listbtn");
 
-var searchButton = document.querySelector("#searchbtn");
-var searchInput = document.querySelector("#search_bar");
+    citiesDiv.appendChild(buttonEL);
+    listClicker();
+  }
+}
+
+//on click function for search history buttons
+function listClicker(){
+  $(".listbtn").on("click", function(event){
+    console.log("anybody home")
+    event.preventDefault();
+    console.log("hello");
+    city = $(this).text().trim();
+    APIcalls();
+  })
+}
+
+//on click function for main search button
+
+function searchClicker() {
+  $("#searchbtn").on("click", function(event){
+    event.preventDefault();
+    city=$(this).prev().val().trim()
+
+    //push the user enterd citi into the cities array
+    cities.push(city);
+    //city array.lengh never exceed 8
+    if(cities.length > 8){
+      cities.shift()
+    }
+    if (city ==""){
+      return;
+    }
+    APIcalls();
+    storeCities();
+    renderButtons();
+  })
+}
+
+//API calls  var apiKey = "bbaae16b1501b9e43700f50808acd154";
+
+function APIcalls(){
+  url = "https://api.openweathermap.org/data/2.5/forecast?q=";
+  currenturl = "https://api.openweathermap.org/data/2.5/weather?q=";
+  APIkey = "&appid=bbaae16b1501b9e43700f50808acd154";
+  queryurl = url + city + APIkey;
+  current_weather_url = currenturl + city + APIkey;
+  $("#name_of_city").text("Today's Weather in " + city);
+  $.ajax({
+    url: queryurl,
+    method: "GET",
+  }).then(function (response) {
+    let day_number = 0;
+
+    //iterate through the 40 weather data sets
+    for (let i = 0; i < response.list.length; i++) {
+      //split function to isolate the time from the time/data aspect of weather data, and only select weather reports for 3pm
+      if (response.list[i].dt_txt.split(" ")[1] == "15:00:00") {
+        //if time of report is 3pm, populate text areas accordingly
+        let day = response.list[i].dt_txt.split("-")[2].split(" ")[0];
+        let month = response.list[i].dt_txt.split("-")[1];
+        let year = response.list[i].dt_txt.split("-")[0];
+        $("#" + day_number + "date").text(month + "/" + day + "/" + year);
+        let temp = Math.round(
+          ((response.list[i].main.temp - 273.15) * 9) / 5 + 32
+        );
+        $("#" + day_number + "five_day_temp").text(
+          "Temp: " + temp + String.fromCharCode(176) + "F"
+        );
+        $("#" + day_number + "five_day_humidity").text(
+          "Humidity: " + response.list[i].main.humidity
+        );
+        $("#" + day_number + "five_day_icon").attr(
+          "src",
+          "http://openweathermap.org/img/w/" +
+            response.list[i].weather[0].icon +
+            ".png"
+        );
+        console.log(response.list[i].dt_txt.split("-"));
+        console.log(day_number);
+        console.log(response.list[i].main.temp);
+        day_number++;
+      }
+    }
+  });
+
+  //function to display data in main div
+  $.ajax({
+    url: current_weather_url,
+    method: "GET",
+  }).then(function (current_data) {
+    console.log(current_data);
+    let temp = Math.round(((current_data.main.temp - 273.15) * 9) / 5 + 32);
+    console.log("The temperature in " + city + " is: " + temp);
+    $("#today_temp").text(
+      "Temperature: " + temp + String.fromCharCode(176) + "F"
+    );
+    $("#today_humidity").text("Humidity: " + current_data.main.humidity);
+    $("#today_wind_speed").text("Wind Speed: " + current_data.wind.speed);
+    $("#today_icon_div").attr({
+      src:
+        "http://openweathermap.org/img/w/" +
+        current_data.weather[0].icon +
+        ".png",
+      height: "100px",
+      width: "100px",
+    });
+  });
+}
+
+
+
+
+
   
-  function getApi() {
-    var requestUrl =
-      "https://api.openweathermap.org/data/2.5/forecast?lat=51.5085&lon=-0.1257&appid=bbaae16b1501b9e43700f50808acd154";
+  
+  
+  
+  
+  // function getApi(){
+  //   var requestUrl =
+  //     "https://api.openweathermap.org/data/2.5/forecast?lat=51.5085&lon=-0.1257&appid=bbaae16b1501b9e43700f50808acd154";
 
-    fetch(requestUrl)
-      .then(function (response) {
-        return response.json();
-      })
-      .then(function (data) {
-        console.log(data);
-      });
-  }
-  getApi()
+  //   fetch(requestUrl)
+  //     .then(function (response) {
+  //       return response.json();
+  //     })
+  //     .then(function (data) {
+  //       console.log(data);
+  //     });
+  // }
+  // getApi()
 
 
 
-  function getCity() {
-    var requestUrl =
-      "https://api.openweathermap.org/data/2.5/forecast?q=London&appid=bbaae16b1501b9e43700f50808acd154";
+  // function getCity() {
+  //   var requestUrl =
+  //     "https://api.openweathermap.org/data/2.5/forecast?q=London&appid=bbaae16b1501b9e43700f50808acd154";
 
-    fetch(requestUrl)
-      .then(function (response) {
-        return response.json();
-      })
-      .then(function (data) {
-        console.log(data);
-      });
-  }
-  getCity()
+  //   fetch(requestUrl)
+  //     .then(function (response) {
+  //       return response.json();
+  //     })
+  //     .then(function (data) {
+  //       console.log(data);
+  //     });
+  // }
+  // getCity()
 
 
 
